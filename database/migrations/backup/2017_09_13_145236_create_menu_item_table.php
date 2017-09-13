21d0ebd8e74d34d6391efc625e286b7b2de59c09
drchoi2k <drchoi2k@naver.com>
2017-09-14 00:18:07 +0900

    20170913

---
+++ database/migrations/backup/2017_09_13_145236_create_menu_item_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_item', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_item');
+    }
+}
--- resources/views/jquery/jquery.blade.php
+++ resources/views/jquery/jquery.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-	
+	@include('jquery.test')
 	@include('jquery.update')
 	<head>
 		<meta name="csrf-token" content="{{ csrf_token() }}">
