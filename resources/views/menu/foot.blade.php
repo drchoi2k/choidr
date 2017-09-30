04559543f2708992a7c1fdfbdb84c3f7709cf27e
CHOI.DR <drchoi2k@naver.com>
2017-09-30 14:07:58 +0900

    no message

---
+++ resources/views/menu/foot.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+			<div class="w3-center w3-small w3-opacity" style='margin-left:220px;'>
+			W3Schools is optimized for learning, testing, and training.
+			Tutorials, references, and examples are constantly reviewed to.<br>
+			While using this site, you agree to have read and accepted our <a href="/about/about_copyright.asp">terms of use</a>,
+			<a href="/about/about_privacy.asp">cookie and privacy policy</a>
+			<a href="/about/about_copyright.asp">Copyright 1999-2017</a> by Refsnes Data. All Rights Reserved.<br>
+			<a href="//www.w3schools.com/w3css/">Powered by W3.CSS</a>.<br><br>
+			<a href="//www.w3schools.com">
+			<img style="width:150px;height:28px;border:0" src="/image/w3schoolscom_gray.gif" alt="W3Schools.com"></a>
+		</div>
+</body>
+</html>
